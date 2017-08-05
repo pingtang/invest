28c589004fa858e61311b2f9ecc31c8e3fa686f1
Cloud User <ec2-user@ip-172-31-13-178.us-west-2.compute.internal>
2017-08-05 00:14:31 -0400

    process sql in mysql

--- docker/webapp/bin/run_webapp.sh
+++ docker/webapp/bin/run_webapp.sh
@@ -45,8 +45,10 @@ __UID=`id -u`
 if [ "${__UID}" -eq 1000 ]
 then
     printf "AWS"
+    printf "Install wordpress plugin: DbTable to DataTable"
     sudo chown root:root /invest/postgres/data
     sudo chmod 777 /invest/postgres/data
+    sudo chown root:root /invest/mysql/
     sudo chmod 777 /invest/mysql/
 
 fi
@@ -54,5 +56,6 @@ fi
 cp ${__WEBAPP_ROOT}/sample/* /invest/postgres/data/
 cp ${__WEBAPP_ROOT}/sample/* /invest/mysql/
 docker exec -it postgres psql -U postgres -d postgres -f ./var/lib/postgresql/data/demo.sql
+docker exec -i mysql mysql -u root -pexample < /invest/mysql/demo_sql.sql
 printf "Done! Check out http:// to see your running webapp.\n"
 
---
+++ docker/webapp/sample/demo_mysql.sql
@@ -0,0 +1,26 @@
+--docker exec -it postgres psql -U postgres -d postgres -f ./var/lib/postgresql/data/demo.sql
+--vi /gpfs/postgres/prod/data/unusual-options-activity-stocks-07-26-2017.csv
+--vi /gpfs/postgres/prod/data/candidate.csv
+--vi /gpfs/postgres/prod/data/results.json
+--copy (select row_to_json(t) from ( select * from tab1) t) to '/var/lib/postgresql/data/tab1.json';
+
+use wordpress;
+drop table candidate;
+create table candidate (symbol char(6));
+LOAD DATA LOCAL INFILE '/var/lib/mysql/candidate.csv'  INTO TABLE candidate  FIELDS TERMINATED BY ',' LINES TERMINATED BY '\n';
+
+drop table uohistory;
+create table uohistory( Symbol char(6), Price numeric, Type char(4), Strike numeric, ExpDate varchar(12), DTE int, Bid numeric,  Midpoint numeric, Ask numeric, Last numeric, Volume int, OpenInterest int, Ratio numeric, IV char(10), Time varchar(12));
+truncate uohistory;
+--select * from uohistory;
+LOAD DATA LOCAL INFILE '/var/lib/mysql/unusual-options-activity-stocks-07-26-2017.csv'  INTO TABLE uohistory FIELDS TERMINATED BY ',' LINES TERMINATED BY '\n';
+--UPDATE table SET ExpDate = str_to_date(ExpDate, '%m/%d/%Y');
+
+drop table results;
+create table results( Symbol char(6), Price numeric, Type char(4), Strike numeric, ExpDate char(12), DTE int, Bid numeric,  Midpoint numeric, Ask numeric, Last numeric, Volume int, OpenInterest int, Ratio numeric, IV char(10), Time char(12));
+insert into results
+select a.* from uohistory a, candidate b where a.Symbol = b.Symbol and a.ratio >= 10 ORDER by a.symbol, a.ratio DESC; 
+--INTO OUTFILE "/var/lib/mysql/results.json"
+--FIELDS TERMINATED BY ','
+--ENCLOSED BY '"'
+--LINES TERMINATED BY '\n';
