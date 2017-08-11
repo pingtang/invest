77f5affb5c6114d26d4e9d3305d9a2c3a42b43c7
Cloud User <ec2-user@ip-172-31-13-178.us-west-2.compute.internal>
2017-08-11 11:41:30 -0400

    x

--- docker/webapp/bin/download_unusual_option.sh
+++ docker/webapp/bin/download_unusual_option.sh
@@ -7,6 +7,7 @@ printf "${__SCRIPT}\n"
 printf "${__BIN}\n"
 printf "${__WEBAPP_ROOT}\n"
 
+cp ${__WEBAPP_ROOT}/sample/*.iim /invest/imacros/Macros
 rm -rf /invest/imacros/Downloads/unusual-options-activity-stocks-*.csv
 docker exec -it imacros /usr/bin/firefox imacros://run/?m=login.iim
 cp /invest/imacros/Downloads/unusual-options-activity-stocks-*.csv ${__WEBAPP_ROOT}/sample/unusual_option/unusual-options-activity-stocks.csv
