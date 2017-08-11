a1ec1f3b7b417b0ce4d74b7c1b851f21a9a21439
Cloud User <ec2-user@ip-172-31-13-178.us-west-2.compute.internal>
2017-08-11 00:24:05 -0400

    x

--- docker/webapp/bin/load_unusual_option.sh
+++ docker/webapp/bin/load_unusual_option.sh
@@ -7,23 +7,8 @@ printf "${__SCRIPT}\n"
 printf "${__BIN}\n"
 printf "${__WEBAPP_ROOT}\n"
 
-python "${__WEBAPP_ROOT}/fileserver/genFile.py"
-python "${__WEBAPP_ROOT}/fileserver/webScraper.py"
-
-cp /invest/archive/*.csv /invest/mysql/
-cp ${__WEBAPP_ROOT}/sample/* /invest/postgres/data/
-cp ${__WEBAPP_ROOT}/sample/* /invest/mysql/
 cp ${__WEBAPP_ROOT}/sample/unusual_option/unusual-options-activity-stocks.csv /invest/mysql/
 cp ${__WEBAPP_ROOT}/sample/*.iim /invest/imacros/Macros
 
 #docker exec -it postgres psql -U postgres -d postgres -f ./var/lib/postgresql/data/demo.sql
 docker exec -i mysql mysql -u root -pexample --force < /invest/mysql/demo_mysql.sql
-
-#load all tickers to database
-TICKERS=`cat ../fileserver/tickerlist`
-for TICKER in $TICKERS; do
-   echo "$TICKER"
-   docker exec -i mysql mysql -u root -pexample --force < /invest/mysql/${TICKER}_demo_load.sql
-done
-
-
