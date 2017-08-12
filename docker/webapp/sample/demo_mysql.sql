8f25e71f8c3eca29d8f18c15c600c45332620570
Ping Tang <ptang@lx-pe-nt42-4.local>
2017-08-12 11:23:01 -0500

    x

--- docker/webapp/sample/demo_mysql.sql
+++ docker/webapp/sample/demo_mysql.sql
@@ -10,14 +10,14 @@ create table candidate (symbol char(6));
 LOAD DATA LOCAL INFILE '/var/lib/mysql/tickerlist.csv'  INTO TABLE candidate  FIELDS TERMINATED BY ',' LINES TERMINATED BY '\n';
 
 drop table uohistory;
-create table uohistory( Symbol char(6), Price numeric, Type char(4), Strike numeric, ExpDate varchar(12), DTE int, Bid numeric,  Midpoint numeric, Ask numeric, Last numeric, Volume int, OpenInterest int, Ratio numeric, IV char(10), Time varchar(12));
+create table uohistory( Symbol char(6), Price numeric, Type char(4), Strike numeric, ExpDate varchar(12), DTE int, Bid float,  Midpoint float, Ask float, Last float, Volume int, OpenInterest int, Ratio numeric, IV char(10), Time varchar(12));
 truncate uohistory;
 --select * from uohistory;
 LOAD DATA LOCAL INFILE '/var/lib/mysql/unusual-options-activity-stocks.csv'  INTO TABLE uohistory FIELDS TERMINATED BY ',' LINES TERMINATED BY '\n';
 --UPDATE table SET ExpDate = str_to_date(ExpDate, '%m/%d/%Y');
 
 drop table results;
-create table results( Symbol char(6), Price numeric, Type char(4), Strike numeric, ExpDate char(12), DTE int, Bid numeric,  Midpoint numeric, Ask numeric, Last numeric, Volume int, OpenInterest int, Ratio numeric, IV char(10), Time char(12));
+create table results( Symbol char(6), Price numeric, Type char(4), Strike numeric, ExpDate char(12), DTE int, Bid float,  Midpoint float, Ask float, Last float, Volume int, OpenInterest int, Ratio numeric, IV char(10), Time char(12));
 insert into results
 select a.* from uohistory a, candidate b where a.Symbol = b.Symbol and a.ratio >= 10 ORDER by a.symbol, a.ratio DESC; 
 --INTO OUTFILE "/var/lib/mysql/results.json"
